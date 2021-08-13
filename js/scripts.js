a3878d48901bc24434a4f9e71bdb3404f662632f
Paige Tiedeman <paigetiedeman@gmail.com>
2021-08-13 16:55:50 -0700

    final formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,34 @@
-function changeColor() {
-  let color = $("#favcolor").val();
-  $.body.style.backgroundColor = color;
-}
-
-$(document).ready(function() {
-    $("#swift").hide();
-    $("#python").hide();
-    $("#javascript").hide();
-    $("#offGrid").hide();
-  $("form#profile").submit(function(event) {
+$(document).ready(function () {
+  $("#swift").hide();
+  $("#python").hide();
+  $("#javascript").hide();
+  $("#offGrid").hide();
+  $("form#profile").submit(function (event) {
     const nameInput = $("input#name").val();
-    
+
+    // Bug: must find new feature to refresh after click as "name" gets duplicated
     $(".name").append(nameInput);
     const phone = $("select#phone").val();
-  
+
     if (phone == "apple") {
       $("#swift").fadeToggle(3000);
-    } if (phone == "samsung") {
+    }
+    if (phone == "samsung") {
       $("#python").slideToggle(2000);
-    } if (phone == "google") {
+    }
+    if (phone == "google") {
       $("#javascript").show();
-    } if (phone == "none") {
-      alert("Did you mean to take this quiz?")
+    }
+    if (phone == "none") {
+      alert("Did you mean to take this quiz?");
       $("#offGrid").show();
     }
     event.preventDefault();
-    
   });
 });
 
-$(document).ready(function(){
-  $("#bg").change(function(){
-    $(".jumbotron").css("background-color",$("#bg").val());
+$(document).ready(function () {
+  $("#bg").change(function () {
+    $(".jumbotron").css("background-color", $("#bg").val());
   });
 });
